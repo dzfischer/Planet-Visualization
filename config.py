28fe227805466726c04128e9f5a6f353b1e1b566
Ryan Joyce <ryan.joyce.88@gmail.com>
2019-08-19 21:16:13 -0400

    update configuration

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:South8*paw@localhost:5432/consumption'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'postgresql://postgres:******@localhost:5432/consumption'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
