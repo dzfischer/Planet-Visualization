ebb15fce43fa49e142d19b83674d0c6ef14bde16
DZF <dzfischer@gmail.com>
2019-08-21 21:16:02 -0400

    start of merging biodiv template

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config.from_object(Config)
+# original app configuration from config file
+# app.config.from_object(Config)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///static/data/planets.sqlite"
 db = SQLAlchemy(app)
 # db.Model.metadata.reflect(db.engine)
 
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,18 @@ from datetime import datetime, date
 from app import app, db
 
 
+
+# --- imported from biodiversity -----
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+planets_table = Base.classes.planets
+
+
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
     if isinstance(obj, (datetime, date)):
@@ -10,6 +22,7 @@ def json_serial(obj):
     raise TypeError("Type %s not serializable" % type(obj))
 
 @app.route('/')
+
 @app.route('/index')
 def home():
     return render_template('index.html', title='Planets!')
@@ -18,7 +31,13 @@ def home():
 def json_stats():
     return 'hello'
 
-
 @app.route('/json_monthly')
 def json_monthly():
     return 'testing'
+
+@app.route('/column/<title>')
+def column(title):
+
+
+
+    return 0;
