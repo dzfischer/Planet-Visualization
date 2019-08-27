daa892d1f2d159237e0a8461f19e1c4629f73368
DZF <dzfischer@gmail.com>
2019-08-26 20:40:56 -0400

    general graph progress

--- app/static/js/general_graph.js
+++ app/static/js/general_graph.js
@@ -14,9 +14,9 @@ function buildCharts(xaxis, yaxis) {
 
   // Building a (somewhat redundant) bubble plot
   let trace = {
-    x: xvals
+    x: xvals,
     y: yvals
-    mode: 'markers',
+    // mode: 'markers',
     // hovertext: sampleData.otu_labels,
     // marker: {
     //   size: sampleData.sample_values,
@@ -41,34 +41,34 @@ function buildCharts(xaxis, yaxis) {
 buildCharts("pl_massj", "st_mass")
 // ------------
 
-function init() {
-  // Grab a reference to the dropdown select elements
-  var selector1 = d3.select("#selDataset1");
-  var selector2 = d3.select("#selDataset2");
+// function init() {
+//   // Grab a reference to the dropdown select elements
+//   var selector1 = d3.select("#selDataset1");
+//   var selector2 = d3.select("#selDataset2");
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector1
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-      selector2
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+//   // Use the list of sample names to populate the select options
+//   d3.json("/names").then((sampleNames) => {
+//     sampleNames.forEach((sample) => {
+//       selector1
+//         .append("option")
+//         .text(sample)
+//         .property("value", sample);
+//       selector2
+//         .append("option")
+//         .text(sample)
+//         .property("value", sample);
+//     });
 
-    // Use the first sample from the list to build the initial plots
-    // const firstSample = sampleNames[0];
-    // buildCharts(firstSample, firstSample);
-  });
-}
+//     // Use the first sample from the list to build the initial plots
+//     // const firstSample = sampleNames[0];
+//     // buildCharts(firstSample, firstSample);
+//   });
+// }
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  // buildCharts(newSample);
-}
+// function optionChanged(newSample) {
+//   // Fetch new data each time a new sample is selected
+//   // buildCharts(newSample);
+// }
 
 // Initialize the dashboard
-init();
+// init();
